1acbd97e34275bcd545b5fd9d95baa1daea78363
大猫 <zlyxzq@163.com>
2022-08-18 21:37:14 +0800

    [新增功能](master): 通用关键字
    定义ctxUser和ctxClientId常量.

--- src/main/java/com/catface/common/enums/common/CommonConst.java
+++ src/main/java/com/catface/common/enums/common/CommonConst.java
@@ -12,6 +12,10 @@ public class CommonConst {
 
   public static final String USER_ID = "userId";
 
+  public static final String CTX_USER_ID = "ctxUserId";
+
+  public static final String CTX_CLIENT_ID = "ctxClientId";
+
   public static final String USER_IDS = "userIds";
 
   public static final String APP_ID = "appId";
