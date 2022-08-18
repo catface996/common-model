29031386109f9576d71dfe09af607b1a71577dc1
大猫 <zlyxzq@163.com>
2022-08-18 21:39:30 +0800

    [新增功能](master): 关键字
    常量token

--- src/main/java/com/catface/common/enums/common/CommonConst.java
+++ src/main/java/com/catface/common/enums/common/CommonConst.java
@@ -6,20 +6,22 @@ package com.catface.common.enums.common;
  */
 public class CommonConst {
 
-  public static final String HM_TOKEN = "token";
+    public static final String HM_TOKEN = "token";
 
-  public static final String ACCOUNT_ID = "accountId";
+    public static final String ACCOUNT_ID = "accountId";
 
-  public static final String USER_ID = "userId";
+    public static final String USER_ID = "userId";
 
-  public static final String CTX_USER_ID = "ctxUserId";
+    public static final String CTX_USER_ID = "ctxUserId";
 
-  public static final String CTX_CLIENT_ID = "ctxClientId";
+    public static final String CTX_CLIENT_ID = "ctxClientId";
 
-  public static final String USER_IDS = "userIds";
+    public static final String USER_IDS = "userIds";
 
-  public static final String APP_ID = "appId";
+    public static final String APP_ID = "appId";
 
-  public static final String OPEN_ID = "openId";
+    public static final String OPEN_ID = "openId";
+
+    public static final String TOKEN = "token";
 
 }
